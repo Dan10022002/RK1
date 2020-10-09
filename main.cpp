5ff90bf062a3c218b3cd5e88dfad0eb3eec897ee
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2020-10-09 18:58:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ int main()
     std::string S;
     bool ccc;
     std::cout << "Enter your text:\n";
-    std::getline(std::cin, S);//Вводим наш текстб при этом пробел не является символом-терминатором
+    std::getline(std::cin, S);
     int t = 0;
     for (int i=65;i<=90;++i)
     {
         long a = count(S.begin(),S.end(), static_cast<char>(i));
-        std::cout << "The part of " << static_cast<char> (i) << " is " << a << std::endl;//Находим количество каждой буквы в тексте
-        t = t + a;//Переменная t необходима для вычисление количества всех букв, не вулючая иные символы
+        std::cout << "The part of " << static_cast<char> (i) << " is " << a << std::endl;
+        t = t + a;
     }
-    std::cout << "The number of articles is " << t << std::endl;//Выводим количество букв на экран, это необходимо для подсчёта частоты букв
+    std::cout << "The number of articles is " << t << std::endl;
     do {
         std::cout << "Enter a letter: ";
         char x;
